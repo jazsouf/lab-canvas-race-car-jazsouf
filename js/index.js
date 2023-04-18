eca0754f3d2c31de911313599328797fa55715da
jazsouf <101473962+jazsouf@users.noreply.github.com>
2023-04-18 16:17:19 +0200

    Draw and place car in canvas

--- js/index.js
+++ js/index.js
@@ -9,8 +9,22 @@ window.onload = () => {
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+// draw road
 const road = new Image();
+road.src = "../images/road.png";
 road.onload = function () {
   ctx.drawImage(road, 0, 0);
 };
-road.src = "../images/road.png";
+
+// draw car
+const car = new Image();
+car.src = "../images/car.png";
+car.onload = function () {
+  ctx.drawImage(
+    car,
+    road.width / 2 - car.width / 7,
+    road.height - car.height / 3,
+    car.width * 0.3,
+    car.height * 0.3
+  ); // how does the width / 7 work ? I would think it should be width / 2...
+};
