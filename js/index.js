25f40ffb86037e63e38b220162bdc34bc878e709
jazsouf <101473962+jazsouf@users.noreply.github.com>
2023-04-18 15:58:04 +0200

    Draw road image

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
   function startGame() {}
 };
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const road = new Image();
+road.onload = function () {
+  ctx.drawImage(road, 0, 0);
+};
+road.src = "../images/road.png";
